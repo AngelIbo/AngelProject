5ed9a3962adb67e0d954afbcbf5f7af43bdde764
AngelIbo <iboangel64@gmail.com>
2025-07-29 14:02:23 +0800

    MAO NANI FINAL

---
+++ src/angelproject/Angelproject2.java
@@ -0,0 +1,22 @@
+
+package angelproject;
+
+import java.util.Scanner;
+
+public class Angelproject2 {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number == 0) {
+            System.out.println("Inputted number Zero");
+        } else if (number % 2 == 0) {
+            System.out.println("Inputted number is even");
+        } else {
+            System.out.println("Inputted number is odd");
+        }
+        sc.close();
+    }
+}
